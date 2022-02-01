fb54bb31fb471eaf76cd903215030cc6e6f84046
MillyP <millypalmer1@hotmail.co.uk>
2022-02-01 16:48:33 +0000

    Added HTML templates and get functions for routes
    Co-authored-by: Orian <77785778+OrianP@users.noreply.github.com>
    Co-authored-by: Paolo <15671939+paologhidoni@users.noreply.github.com>

---
+++ routes/profile.js
@@ -0,0 +1,38 @@
+
+
+function get (req, res) {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Pets App Profile</title>
+    </head>
+    <body>
+        
+        <div class="wrapper">
+            <h1>Hello email</h1>
+        <div class="links">  
+        <a href="/" aria-label="Return to Home Page">Return to Home Page</a>
+        
+        <form method="POST" action="/log-out">
+            <button type="submit">Log Out</button>
+        </form>
+        </div>
+    
+        <form enctype="multipart/form-data" method="POST" action="/add-post">
+        <label for="caption">Caption your pet</label>
+        <input name="caption" id="caption" type="text" aria-label="Caption your Pet">
+        <label for="image">Post Image of your pet</label>
+        <input name="image" id="image" type="file" aria-label="Post image of your pet">
+        <button type="submit" aria-label="Click button to Submit">Submit</button>
+        </form>
+        </div>
+    </body>
+    </html>`
+    res.send(html)
+}
+
+module.exports = {get}
