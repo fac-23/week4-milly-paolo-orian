a9457d9d056b55c46968e19bf7a4854c2d1afba8
MillyP <millypalmer1@hotmail.co.uk>
2022-02-02 17:42:54 +0000

    Wrote tests for homepage, sign up, log in links, css. Changed app name in homepage
    Co-authored-by: Orian <77785778+OrianP@users.noreply.github.com>
    Co-authored-by: Paolo <15671939+paologhidoni@users.noreply.github.com>

--- routes/home.js
+++ routes/home.js
@@ -1,29 +1,25 @@
-
-
-function get (req, res) {
-    const html = `<!DOCTYPE html>
+function get(req, res) {
+  const html = `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
-        <title>Pets App</title>
+        <title>Petarazzi</title>
     </head>
     <body>
         <div class="wrapper">
-            <h1>Pet App</h1>
-            <img class="logo" src="#" alt="Pets App Logo">
+            <h1>Petarazzi</h1>
+            <img class="logo" src="#" alt="Petarazzi">
         <div class="links">  
-            <a href="/sign-up" aria-label="Click this link to sign up">Sign Up</a>
+            <a href="/sign-up" aria-label="Click this link to sign up">Sign up</a>
         <a href="/log-in" aria-label="Click this link to log in">Log in</a>
         </div> 
         </div>
     </body>
-    </html>`
-    res.send(html)
+    </html>`;
+  res.send(html);
 }
 
-
-
-module.exports = {get}
+module.exports = { get };
