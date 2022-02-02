95fd554fefaa3171d86e2269b83bf8cb83584644
orian <77785778+OrianP@users.noreply.github.com>
2022-02-02 17:01:45 +0000

    Add /add-post post function, /news-feed get function
    Co-authored-by: Paolo <15671939+paologhidoni@users.noreply.github.com>
    Co-authored-by: Milly <62570785+millipede-cpu@users.noreply.github.com>

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const logIn = require("./routes/logIn.js");
 const logOut = require("./routes/logOut.js");
 const profile = require("./routes/profile.js");
 // const addPost = require("./routes/addPost.js");
-const newsFeed = require("./routes/newsFeed.js");
+// const newsFeed = require("./routes/newsFeed.js");
 
 // Home //
 // Display sign up and log in links
@@ -59,12 +59,93 @@ server.post("/log-out", logOut.post);
 // // Profile //
 // // serve profile page with form
 server.get("/profile", checkAuth, profile.get);
+
+//  ADD POST //
 // // Process user input into database and redirect to newsfeed
 // server.post("/add-post", checkAuth, addPost.post);
+const multer = require("multer");
+const imgUpload = multer();
+
+const model = require("./database/model.js");
+
+server.post(
+  "/add-post",
+  checkAuth,
+  imgUpload.single("image"),
+  (req, res, next) => {
+    // get user inputs from req.body
+    const { caption } = req.body;
+    const img = req.file;
+    const userId = req.session.userObj.id;
+    //  insert caption and img into DB posts table
+    return model
+      .createPost(caption, img.buffer, userId)
+      .then(() => {
+        res.redirect("/news-feed");
+      })
+      .catch((error) => {
+        console.error(error);
+        next(error);
+      });
+  }
+);
 
 // // Newsfeed //
-// // Gets all the posts from database and displays them
-server.get("/news-feed", checkAuth, newsFeed.get);
+// // Gets all the posts from database and display them
+server.get("/news-feed", (req, res) => {
+  const user = req.session;
+  model.getPosts().then((result) => {
+    // create array of posts
+    const posts = result
+      .map((post) => {
+        return `<li>
+      <p>${user.userObj.username}</p>
+      <p>${post.caption}</p>
+      
+      ${
+        post.img
+          ? `<img src="/post/${post.id}/img" alt="${post.caption}" width="64" height="64">`
+          : ""
+      }
+      </li>`;
+      })
+      .reverse()
+      .join("");
+
+    const html = `
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Pets App News Feed</title>
+  </head>
+  <body>
+    <div class="wrapper">
+      <h1>Pets App News Feed</h1>
+      <div class="links">
+        <a href="/" aria-label="Return to Home Page">Return to Home Page</a>
+        <a href="/profile" aria-label="Return to Profile">Return to Profile</a>
+      </div>
+      
+      <ul>
+    ${posts}
+      </ul>
+    </div>
+  </body>
+</html>`;
+
+    res.send(html);
+  });
+});
+
+server.get("/post/:id/img", (req, res) => {
+  model.getImg(req.params.id).then((user) => {
+    res.send(user.img);
+  });
+});
 
 // assign port to deployed or local port
 // eslint-disable-next-line no-undef
